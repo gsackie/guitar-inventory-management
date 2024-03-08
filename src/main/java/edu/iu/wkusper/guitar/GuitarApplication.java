516ded959fe5d0a3ca5048d8c4a96a06191e61a3
Will Kusper <wkusper@iu.edu>
2024-03-08 17:35:48 -0500

    Homework 6

--- src/main/java/edu/iu/wkusper/guitar/GuitarApplication.java
+++ src/main/java/edu/iu/wkusper/guitar/GuitarApplication.java
@@ -1,9 +1,12 @@
 package edu.iu.wkusper.guitar;
 
+import edu.iu.wkusper.guitar.model.Guitar;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GuitarApplication {
 
 	public static void main(String[] args) {
